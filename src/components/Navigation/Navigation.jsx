7eef81f9966a4ee32ce7003c8b05159b0d9cdafe
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-06 19:40:17 +0100

    navigation to products and fetching and filtering products

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useSearchParams, useNavigate } from "react-router-dom";
+import styles from "./Navigation.module.css";
+
 const Navigation = () => {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    navigate("/products", { state: { query: search } });
+  };
   return (
-    <div>
-      <div>Navigation</div>
+    <>
+      <div className={styles.navigation}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search..."
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
       <Outlet />
-    </div>
+    </>
   );
 };
 
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useSearchParams, useLocation } from "react-router-dom";
 
 const Products = () => {
-  return <div>Products</div>;
+  const location = useLocation();
+  const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filterProducts = (products, searchTerm) => {
+    return products.filter((product) => {
+      return product.title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  };
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      const searchTerm = location.state.query;
+      setSearchParams({ search: searchTerm });
+
+      const filteredProducts = filterProducts(products, searchTerm);
+      console.log(filteredProducts);
+      setProducts(filteredProducts);
+    })();
+  }, []);
+  return <div>{JSON.stringify(products)}</div>;
 };
 
 export default Products;
