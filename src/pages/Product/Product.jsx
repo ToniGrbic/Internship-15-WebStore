abbc8cedee85f5e67b9de0dd4de9432d2db8b322
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 17:56:45 +0100

    product details page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import styles from "./Product.module.css";
 
 const Product = () => {
   const { id } = useParams();
+  const location = useLocation();
+  const product = location.state;
 
-  return <div>Product: {id}</div>;
+  return (
+    <div className={styles["product-container"]}>
+      <div className={styles["product-image"]}>
+        <img src={product.image} alt={product.title} />
+      </div>
+      <div className={styles["product-details"]}>
+        <h2>{product.title}</h2>
+        <p>Category: {product.category}</p>
+        <p>Price: ${product.price}</p>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
