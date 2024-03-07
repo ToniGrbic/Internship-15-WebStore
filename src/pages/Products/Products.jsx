4ab1b6d04868a1e9d38e91568cb3d33708b5127d
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 20:21:23 +0100

    refactored

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useSearchParams, useLocation, useNavigate } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
+import ProductCard from "../../components/ProductCard";
 import styles from "./Products.module.css";
 
-const Products = () => {
+const Products = ({ products }) => {
   const location = useLocation();
-  const navigate = useNavigate();
-
-  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const filterProducts = (products, searchTerm) => {
@@ -15,38 +14,19 @@ const Products = () => {
       return product.title.toLowerCase().includes(searchTerm.toLowerCase());
     });
   };
+
   useEffect(() => {
-    (async () => {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const products = await response.json();
-      const searchTerm = location.state.query;
-      setSearchParams({ search: searchTerm });
+    const searchTerm = location.state.query;
+    setSearchParams({ search: searchTerm });
 
-      const filteredProducts = filterProducts(products, searchTerm);
-      setProducts(filteredProducts);
-    })();
+    const filteredProducts = filterProducts(products, searchTerm);
+    setFilteredProducts(filteredProducts);
   }, []);
-
-  const handleProductClick = (e, product) => {
-    navigate(`/product/${product.id}`, { state: product });
-  };
 
   return (
     <div className={styles["products-container"]}>
-      {products.map((product) => {
-        return (
-          <div
-            key={product.id}
-            className={styles["product-card"]}
-            onClick={(e) => handleProductClick(e, product)}
-          >
-            <div className={styles["product-image"]}>
-              <img src={product.image} alt={product.title} />
-            </div>
-            <h4>{product.title}</h4>
-            <p>{product.category}</p>
-          </div>
-        );
+      {filteredProducts.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
       })}
     </div>
   );
