7ff1fd4df6fd9965ba6bb1c7cc087145f011b169
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 20:20:36 +0100

    refactored to fetch all products in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
@@ -7,14 +8,28 @@ import Products from "./pages/Products";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      setProducts(products);
+    })();
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route element={<Navigation />}>
             <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/product/:id" element={<Product />} />
+            <Route
+              path="/products"
+              element={<Products products={products} />}
+            />
+            <Route
+              path="/product/:id"
+              element={<Product products={products} />}
+            />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
