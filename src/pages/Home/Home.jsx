60f89c3231688b3564686b39473e3cdf286d289a
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 17:56:28 +0100

    render products titles in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+
+      setProducts(products);
+    })();
+  }, []);
+
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.caregory}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
