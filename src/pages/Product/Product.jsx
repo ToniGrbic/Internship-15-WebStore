a06c56a8d351e286f016e292fb0835edcef23448
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 20:22:43 +0100

    otherProducts section

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,24 +1,44 @@
-import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./Product.module.css";
+import ProductCard from "../../components/ProductCard";
 
-const Product = () => {
-  const { id } = useParams();
+const Product = ({ products }) => {
   const location = useLocation();
-  const product = location.state;
+  const [otherProducts, setOtherProducts] = useState([]);
+  const product = location?.state;
+
+  useEffect(() => {
+    const otherProducts = products.filter((product) => {
+      return product.id !== location?.state?.id;
+    });
+    setOtherProducts(otherProducts);
+  }, [location.state, products]);
 
   return (
-    <div className={styles["product-container"]}>
-      <div className={styles["product-image"]}>
-        <img src={product.image} alt={product.title} />
-      </div>
-      <div className={styles["product-details"]}>
-        <h2>{product.title}</h2>
-        <p>Category: {product.category}</p>
-        <p>Price: ${product.price}</p>
-        <p>{product.description}</p>
-      </div>
-    </div>
+    <>
+      {product && (
+        <>
+          <div className={styles["product-container"]}>
+            <div className={styles["product-image"]}>
+              <img src={product.image} alt={product.title} />
+            </div>
+            <div className={styles["product-details"]}>
+              <h2>{product.title}</h2>
+              <p>Category: {product.category}</p>
+              <p>Price: ${product.price}</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+          <h2>Other products: </h2>
+          <div className={styles["products-container"]}>
+            {otherProducts.map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
