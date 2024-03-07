5686670112dd225651738c17d911b9778e46ccd2
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-07 17:57:41 +0100

    product cards in products route

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useSearchParams, useLocation } from "react-router-dom";
+import { useSearchParams, useLocation, useNavigate } from "react-router-dom";
+import styles from "./Products.module.css";
 
 const Products = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [products, setProducts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -20,11 +23,33 @@ const Products = () => {
       setSearchParams({ search: searchTerm });
 
       const filteredProducts = filterProducts(products, searchTerm);
-      console.log(filteredProducts);
       setProducts(filteredProducts);
     })();
   }, []);
-  return <div>{JSON.stringify(products)}</div>;
+
+  const handleProductClick = (e, product) => {
+    navigate(`/product/${product.id}`, { state: product });
+  };
+
+  return (
+    <div className={styles["products-container"]}>
+      {products.map((product) => {
+        return (
+          <div
+            key={product.id}
+            className={styles["product-card"]}
+            onClick={(e) => handleProductClick(e, product)}
+          >
+            <div className={styles["product-image"]}>
+              <img src={product.image} alt={product.title} />
+            </div>
+            <h4>{product.title}</h4>
+            <p>{product.category}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Products;
