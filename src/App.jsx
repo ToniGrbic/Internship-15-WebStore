4718cc0f0dce3956b9f44a62e6c4caec24dd5037
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-10 13:25:23 +0100

    added loading, error states and try catch block

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Products from "./pages/Products";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [search, setSearch] = useState("");
@@ -16,15 +19,22 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(url);
-      const products = await response.json();
-      setProducts(products);
+      try {
+        const response = await fetch(url);
+        const products = await response.json();
+        setProducts(products);
 
-      const categories = products.reduce((acc, product) => {
-        if (!acc.includes(product.category)) acc.push(product.category);
-        return acc;
-      }, []);
-      setCategories(["All", ...categories]);
+        const categories = products.reduce((acc, product) => {
+          if (!acc.includes(product.category)) acc.push(product.category);
+          return acc;
+        }, []);
+
+        setCategories(["All", ...categories]);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(true);
+      }
     })();
   }, []);
 
@@ -33,7 +43,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Navigation setSearch={setSearch} />}>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={
+                <Home products={products} loading={loading} error={error} />
+              }
+            />
             <Route
               path="/products"
               element={
