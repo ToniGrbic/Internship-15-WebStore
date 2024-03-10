de863802b3e0efab8ddcabe9b1a6604157249d2d
ToniGrbic <tonigrbic.5@gmail.com>
2024-03-10 14:55:26 +0100

    refactored error message state from ternary to conditional retrun

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,33 +4,31 @@ import ProductCard from "../../components/ProductCard";
 import LoadingSpinner from "../../components/LoadingSpinner";
 
 const HomePage = ({ products, isLoading, isError }) => {
+  if (isError) {
+    return <h2>Something went wrong, try again later...</h2>;
+  }
+
   return (
-    <>
-      {isError ? (
-        <h2>Something went wrong, try again later...</h2>
-      ) : (
-        <div className={styles["home-container"]}>
-          <h1>Welcome to the home page!</h1>
-          <p>
-            This is a simple e-commerce site. Search for a product to navigate
-            to the products page where you can filter by category.
-          </p>
-          <h2>All Products:</h2>
-          <div className={styles["products-container"]}>
-            {isLoading ? (
-              <div>
-                <h2 className={styles["loading-text"]}>Loading...</h2>
-                <LoadingSpinner />
-              </div>
-            ) : (
-              products.map((product) => {
-                return <ProductCard key={product.id} product={product} />;
-              })
-            )}
+    <div className={styles["home-container"]}>
+      <h1>Welcome to the home page!</h1>
+      <p>
+        This is a simple e-commerce site. Search for a product to navigate to
+        the products page where you can filter by category.
+      </p>
+      <h2>All Products:</h2>
+      <div className={styles["products-container"]}>
+        {isLoading ? (
+          <div>
+            <h2 className={styles["loading-text"]}>Loading...</h2>
+            <LoadingSpinner />
           </div>
-        </div>
-      )}
-    </>
+        ) : (
+          products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })
+        )}
+      </div>
+    </div>
   );
 };
 
